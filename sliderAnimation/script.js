7db4333e26669cfd3bfa67ab96d287dcdf63226a
itu-itis22-ozturkde22 <ozturkde22@itu.edu.tr>
2023-01-27 23:17:05 +0300

    Added slider animation

---
+++ sliderAnimation/script.js
@@ -0,0 +1,11 @@
+const left = document.getElementById("leftside");
+
+
+//turns width into a percentage
+const handleMove = e => {
+  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
+
+document.onmousemove = e => handleMove(e);
+
+document.ontouchmove = e => handleMove(e.touches[0]);
